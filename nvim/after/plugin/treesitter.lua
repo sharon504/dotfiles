f75e92a722b1d4a484e702422a5173c01dcf299a
Sharon218 <sharonshajan2017@gmail.com>
2024-02-10 14:25:20 +0530

    refreshing

--- nvim/after/plugin/treesitter.lua
+++
@@ -1,6 +0,0 @@
-require("nvim-treesitter.configs").setup{
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        }

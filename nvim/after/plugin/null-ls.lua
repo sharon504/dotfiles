c4e0bad20a7aebe338e1fa41988addb46908081b
Sharon218 <sharonshajan2017@gmail.com>
2024-02-10 16:40:46 +0530

    adding nvim config

---
+++ nvim/after/plugin/null-ls.lua
@@ -0,0 +1,37 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local null_ls = require("null-ls")
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.black.with({
+            filetypes = { "python", },
+        }),
+        null_ls.builtins.diagnostics.flake8.with({
+            filetypes = { "python", },
+        }),
+        null_ls.builtins.formatting.prettier.with({
+            filetypes = { "javascript", "jsx", "css", "typescript", "html", "javascriptreact", "typescriptreact",},
+        }),
+        null_ls.builtins.formatting.stylua.with({
+            filetypes = {"lua",},
+        }),
+        
+    },
+
+    on_attach = function(client, bufnr)
+        if client.supports_method ("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds ({
+                group = augroup,
+                buffer= bufnr,
+            })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer= bufnr,
+                callback = function ()
+                    vim. lsp.buf.format({ bufnr = bufnr })
+                end,
+            })
+        end
+    end,
+})
+
---
+++ nvim/lua/set.lua
@@ -0,0 +1,37 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.opt.number = true
+vim.opt.textwidth=80
+vim.opt.wrapmargin=0
+vim.opt.formatoptions:append("t")
+vim.opt.linebreak = true
+
+vim.opt.clipboard = 'unnamedplus'
+vim.cmd "set noshowmode"

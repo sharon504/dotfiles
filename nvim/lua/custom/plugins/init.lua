a034ea4aaeab627491fabb1d1880ddb14976a53d
sharon504 <sharonsphajan@gmail.com>
2024-04-10 01:45:12 +0530

    Changing neovim configuration

---
+++ nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,22 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = {--[[ things you want to change go here]]
+		},
+	},
+}
---
+++ nvim_backup/after/plugin/treesitter.lua
@@ -0,0 +1,6 @@
+require("nvim-treesitter.configs").setup{
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        }

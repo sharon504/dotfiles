3b4a3b238913d8a9bd2b6dff353d28e0987c637c
sharon504 <sharonpshajan@gmail.com>
2025-02-01 01:51:06 +0530

    Changed auto-save config and fixed bugs in formatting.lua and nvim-tree.lua

--- nvim/lua/plugins/auto-save.lua
+++ nvim/lua/plugins/auto-save.lua
@@ -1,39 +1,127 @@
-return {
-	"Pocco81/auto-save.nvim",
-	config = function()
-		require("auto-save").setup({
-			{
-				enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
-				execution_message = {
-					message = function() -- message to print on save
-						return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
-					end,
-					dim = 0.18, -- dim the color of `message`
-					cleaning_interval = 1000, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-				},
-				trigger_events = { "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
-				-- function that determines whether to save the current buffer or not
-				-- return true: if buffer is ok to be saved
-				-- return false: if it's not ok to be saved
-				condition = function(buf)
-					local fn = vim.fn
-					local utils = require("auto-save.utils.data")
+-- https://github.com/okuuva/auto-save.nvim
+--
+-- This is a fork of original plugin `https://github.com/pocco81/auto-save.nvim`
+-- but the original one was updated 2 years ago, and I was experiencing issues
+-- with autoformat and undo/redo
+--
+-- Filename: ~/github/dotfiles-latest/neovim/neobean/lua/plugins/auto-save.lua
+-- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/auto-save.lua
 
-					if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
-						return true -- met condition(s), can save
-					end
-					return false -- can't save
-				end,
-				write_all_buffers = false, -- write all buffers when the current one meets `condition`
-				debounce_delay = 1000, -- saves the file at most every `debounce_delay` milliseconds
-				callbacks = { -- functions to be executed at different intervals
-					enabling = nil, -- ran when enabling auto-save
-					disabling = nil, -- ran when disabling auto-save
-					before_asserting_save = nil, -- ran before checking `condition`
-					before_saving = nil, -- ran before doing the actual save
-					after_saving = nil, -- ran after doing the actual save
+-- My related YouTube video
+-- Save neovim files automatically with auto-save.nvim
+-- https://youtu.be/W5fjlU4tSpw
+
+-- I had undo/redo issues when using the no longer maintained plugin from pocco81
+-- So make sure you're using the right plugin, which is okuuva/auto-save.nvim
+-- https://github.com/pocco81/auto-save.nvim/issues/70
+
+-- Autocommand for printing the autosaved message
+local group = vim.api.nvim_create_augroup("autosave", {})
+vim.api.nvim_create_autocmd("User", {
+	pattern = "AutoSaveWritePost",
+	group = group,
+	callback = function(opts)
+		if opts.data.saved_buffer ~= nil then
+			-- print("AutoSaved at " .. vim.fn.strftime("%H:%M:%S"))
+			print("AutoSaved")
+		end
+	end,
+})
+
+-- I do not want to save when I'm in visual mode because I'm usually moving
+-- stuff from one place to another, or deleting it
+-- I got this suggestion from the plugin maintainers
+-- https://github.com/okuuva/auto-save.nvim/issues/67#issuecomment-2597631756
+local visual_event_group = vim.api.nvim_create_augroup("visual_event", { clear = true })
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+	group = visual_event_group,
+	pattern = { "*:[vV\x16]*" },
+	callback = function()
+		vim.api.nvim_exec_autocmds("User", { pattern = "VisualEnter" })
+		-- print("VisualEnter")
+	end,
+})
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+	group = visual_event_group,
+	pattern = { "[vV\x16]*:*" },
+	callback = function()
+		vim.api.nvim_exec_autocmds("User", { pattern = "VisualLeave" })
+		-- print("VisualLeave")
+	end,
+})
+
+return {
+	{
+		"okuuva/auto-save.nvim",
+		enabled = true,
+		cmd = "ASToggle", -- optional for lazy loading on command
+		event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+		opts = {
+			enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+			trigger_events = { -- See :h events
+				-- -- vim events that trigger an immediate save
+				-- -- I'm disabling this, as it's autosaving when I leave the buffer and
+				-- -- that's autoformatting stuff if on insert mode and following a tutorial
+				-- -- Re-enabling this to only save if NOT in insert mode in the condition below
+				-- immediate_save = { nil },
+				immediate_save = { "BufLeave", "FocusLost", "QuitPre", "VimSuspend" }, -- vim events that trigger an immediate save
+				-- vim events that trigger a deferred save (saves after `debounce_delay`)
+				defer_save = {
+					"InsertLeave",
+					"TextChanged",
+					{ "User", pattern = "VisualLeave" },
+				},
+				cancel_deferred_save = {
+					"InsertEnter",
+					{ "User", pattern = "VisualEnter" },
 				},
 			},
-		})
-	end,
+			-- function that takes the buffer handle and determines whether to save the current buffer or not
+			-- return true: if buffer is ok to be saved
+			-- return false: if it's not ok to be saved
+			-- if set to `nil` then no specific condition is applied
+			condition = function(buf)
+				-- Do not save when I'm in insert mode
+				-- Do NOT ADD VISUAL MODE HERE or the cancel_deferred_save wont' work
+				-- If I STAY in insert mode and switch to another app, like YouTube to
+				-- take notes, the BufLeave or FocusLost immediate_save will be ignored
+				-- and the save will not be triggered
+				local mode = vim.fn.mode()
+				if mode == "i" then
+					return false
+				end
+
+				-- Disable auto-save for the harpoon plugin, otherwise it just opens and closes
+				-- https://github.com/ThePrimeagen/harpoon/issues/434
+				--
+				-- don't save for `sql` file types
+				-- I do this so when working with dadbod the file is not saved every time
+				-- I make a change, and a SQL query executed
+				-- Run `:set filetype?` on a dadbod query to make sure of the filetype
+				local filetype = vim.bo[buf].filetype
+				if filetype == "harpoon" or filetype == "mysql" then
+					return false
+				end
+
+				-- Skip autosave if you're in an active snippet
+				if require("luasnip").in_snippet() then
+					return false
+				end
+
+				return true
+			end,
+			write_all_buffers = false, -- write all buffers when the current one meets `condition`
+			-- Do not execute autocmds when saving
+			-- If you set noautocmd = true, autosave won't trigger an auto format
+			-- https://github.com/okuuva/auto-save.nvim/issues/55
+			noautocmd = false,
+			lockmarks = false, -- lock marks when saving, see `:h lockmarks` for more details
+			-- delay after which a pending save is executed (default 1000)
+			debounce_delay = 2000,
+			-- log debug messages to 'auto-save.log' file in neovim cache directory, set to `true` to enable
+			debug = false,
+		},
+	},
 }

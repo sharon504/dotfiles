75bdd53bc70a1effa95d0fcd943ff9186eb8e36e
Sharon504 <sharonpshajan@gmail.com>
2025-10-14 19:50:29 +0530

    fixing telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -37,13 +37,19 @@ return {
 		})
 
 		telescope.load_extension("fzf")
+		telescope.load_extension("todo-comments")
+		telescope.load_extension("projects")
 
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		keymap.set("n", "<leader>ff", function()
+			require("telescope.builtin").find_files({ hidden = true })
+		end, { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+		keymap.set("n", "<leader>fs", function()
+			require("telescope.builtin").live_grep({ hidden = true, additional_args = { "--hidden" } })
+		end, { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 		keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, { desc = "Find in help tags" })
